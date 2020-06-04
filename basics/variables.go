c7dbf2c1ad7972e5fe87aaaa4c9520b9f0d6117f
Prakhar Varshney <prakharnegative@gmail.com>
2020-06-04 20:44:20 +0530

    added new

---
+++ basics/variables.go
@@ -0,0 +1,12 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var batman string = "I am batman"
+// 	fmt.Println(batman)
+
+// 	ironman := "I am ironman"
+// 	fmt.Println(ironman)
+
+// }
